7c590fd138c9b809f048d7d199790238a031f62a
Breno Coll <collbreno@dcc.ufrj.br>
2024-05-26 01:28:23 -0300

    implementing cubit tests (using search by query)

--- test/bloc/apod_list_cubit_test.dart
+++ test/bloc/apod_list_cubit_test.dart
@@ -22,19 +22,17 @@ void main() {
       () async {
         repository = MockRepository();
         fix = await ApodListFixture.loadFromMarch();
-      },
-    );
-
-    blocTest(
-      'simple call',
-      build: () => ApodListCubit(repository),
-      setUp: () {
         when(repository.getApods(any)).thenAnswer(
           (invocation) async => fix.fromRange(
             invocation.positionalArguments.single,
           ),
         );
       },
+    );
+
+    blocTest<ApodListCubit, ApodListState>(
+      'simple call',
+      build: () => ApodListCubit(repository),
       act: (bloc) => withClock(
         Clock.fixed(march(30)),
         () async {
@@ -64,16 +62,9 @@ void main() {
       ],
     );
 
-    blocTest(
+    blocTest<ApodListCubit, ApodListState>(
       'three loadings',
       build: () => ApodListCubit(repository),
-      setUp: () {
-        when(repository.getApods(any)).thenAnswer(
-          (invocation) async => fix.fromRange(
-            invocation.positionalArguments.single,
-          ),
-        );
-      },
       act: (bloc) => withClock(
         Clock.fixed(march(30)),
         () async {
@@ -148,5 +139,163 @@ void main() {
         ),
       ],
     );
+
+    group('using search by query', () {
+      blocTest<ApodListCubit, ApodListState>(
+        "when user searches for 'galaxy' while the current list doesn't "
+        "have any 'galaxy', so the result must be an empty list",
+        build: () => ApodListCubit(repository),
+        act: (bloc) => withClock(
+          Clock.fixed(march(30)),
+          () async {
+            await bloc.loadMore();
+            bloc.search('galaxy');
+          },
+        ),
+        verify: (bloc) {
+          expect(
+            bloc.state.filtered,
+            isEmpty,
+          );
+        },
+        expect: () => [
+          const ApodListState(
+            items: [],
+            isLoading: true,
+            error: null,
+            query: '',
+            dateRange: null,
+            infiniteScrollLastDay: null,
+          ),
+          ApodListState(
+            items: fix
+                .fromRange(DateTimeRange(start: march(21), end: march(30)))
+                .reversed
+                .toList(),
+            isLoading: false,
+            error: null,
+            query: '',
+            dateRange: null,
+            infiniteScrollLastDay: march(21),
+          ),
+          ApodListState(
+            items: fix
+                .fromRange(DateTimeRange(start: march(21), end: march(30)))
+                .reversed
+                .toList(),
+            isLoading: false,
+            error: null,
+            query: 'galaxy',
+            dateRange: null,
+            infiniteScrollLastDay: march(21),
+          ),
+        ],
+      );
+
+      blocTest<ApodListCubit, ApodListState>(
+        "when user searches for 'galaxy' while the current list has "
+        "galaxies, the result must contain only galaxies",
+        build: () => ApodListCubit(repository),
+        act: (bloc) => withClock(
+          Clock.fixed(march(20)),
+          () async {
+            await bloc.loadMore();
+            bloc.search('galaxy');
+          },
+        ),
+        verify: (bloc) {
+          expect(
+            bloc.state.filtered,
+            orderedEquals([
+              fix.fromDate(march(20)),
+              fix.fromDate(march(17)),
+              fix.fromDate(march(12)),
+            ]),
+          );
+        },
+        expect: () => [
+          const ApodListState(
+            items: [],
+            isLoading: true,
+            error: null,
+            query: '',
+            dateRange: null,
+            infiniteScrollLastDay: null,
+          ),
+          ApodListState(
+            items: fix
+                .fromRange(DateTimeRange(start: march(11), end: march(20)))
+                .reversed
+                .toList(),
+            isLoading: false,
+            error: null,
+            query: '',
+            dateRange: null,
+            infiniteScrollLastDay: march(11),
+          ),
+          ApodListState(
+            items: fix
+                .fromRange(DateTimeRange(start: march(11), end: march(20)))
+                .reversed
+                .toList(),
+            isLoading: false,
+            error: null,
+            query: 'galaxy',
+            dateRange: null,
+            infiniteScrollLastDay: march(11),
+          ),
+        ],
+      );
+
+      blocTest<ApodListCubit, ApodListState>(
+        "when user searches for 'galaxy' and loads more, the new "
+        "found galaxies must be added to the result",
+        build: () => ApodListCubit(repository),
+        act: (bloc) => withClock(
+          Clock.fixed(march(20)),
+          () async {
+            await bloc.loadMore();
+            bloc.search('galaxy');
+            await bloc.loadMore();
+          },
+        ),
+        verify: (bloc) {
+          expect(
+            bloc.state.filtered,
+            orderedEquals([
+              fix.fromDate(march(20)),
+              fix.fromDate(march(17)),
+              fix.fromDate(march(12)),
+              fix.fromDate(march(6)),
+            ]),
+          );
+        },
+        skip: 3,
+        expect: () => [
+          ApodListState(
+            items: fix
+                .fromRange(DateTimeRange(start: march(11), end: march(20)))
+                .reversed
+                .toList(),
+            isLoading: true,
+            error: null,
+            query: 'galaxy',
+            dateRange: null,
+            infiniteScrollLastDay: march(11),
+          ),
+          ApodListState(
+            items: fix
+                .fromRange(DateTimeRange(start: march(1), end: march(20)))
+                .reversed
+                .toList(),
+            isLoading: false,
+            error: null,
+            query: 'galaxy',
+            dateRange: null,
+            infiniteScrollLastDay: march(1),
+          ),
+        ],
+      );
+    });
   });
 }
